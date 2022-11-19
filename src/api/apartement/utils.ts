c86df7ec9cb8e55af3a17ad49f55ad2376b2e7a3
Aziz <abdelaziz.sbaai@kestio.com>
2022-11-19 14:41:29 +0100

    correct wrong HTTP error code

--- src/api/apartement/utils.ts
+++ src/api/apartement/utils.ts
@@ -2,7 +2,8 @@ import { Response } from 'express'
 
 export const handleError = (res: Response) => (reason: any) => {
   console.dir(reason)
-  res.status(401).send(reason)
+  res.status(400).send(reason)
 }
 export const regExIsZipCode = /[0-9]{5}(?:-[0-9]{4})?/g
 export const regExIsNumber = /^[0-9]+$/
+export const regExIsFloat = /^-?\d+\.?\d*$/
--- src/api/room/utils.ts
+++ src/api/room/utils.ts
@@ -2,7 +2,7 @@ import { Response } from 'express'
 
 export const handleError = (res: Response) => (reason: any) => {
   console.dir(reason)
-  res.status(401).send(reason)
+  res.status(400).send(reason)
 }
 
 export const regExIsNumber = /^[0-9]+$/
